78723561ed33685d12442b5689d3054fb5042a1b
Roman Konstantinovich <krom.xr@gmail.com>
2014-05-17 01:00:13 +0600

    addslashes

--- src/filters.js
+++ src/filters.js
@@ -1,38 +1,95 @@
 /*global angular*/
-angular.module('useful_things.filters', [])
-    .filter('log', function() {
-        return function(ob, is_hide_in_template) {
-            console.log('template log - ', ob);
-            if (is_hide_in_template) { return ''; }
-            return ob;
-        };
-    })
-    .filter('default', function() {
-        return function(value, default_string) {
-            default_string = default_string || "---";
-            if (!value) { return default_string; }
-            return value;
-        };
-    })
-    .filter('linebreak', function() {
-        //break_value - p, div, br
-        return function(text, break_value) { 
-            if (break_value === 'br') { return text.split(/\r|\n/).join("</br>"); }
+(function() {
 
-            var text_arr = text.split(/\r?\n/);
-            var text_arr2 = [];
-            for (var i = 0; i < text_arr.length; i++) {
-                var text_item = text_arr[i];
-                text_arr2.push("<" + break_value + ">" + text_item + "</" + break_value + ">");
+    angular.module('useful_things.filters', [])
+        .filter('log', function() {
+            return function(ob, is_hide_in_template) {
+                console.log('template log - ', ob);
+                if (is_hide_in_template) { return ''; }
+                return ob;
+            };
+        })
+
+        .filter('add', function() {
+            return function(ob, value) {
+                var ob_num = Number(ob), value_num = Number(value);
+                ob = isNaN(ob_num) ? ob : ob_num;
+                value = isNaN(value_num) ? value : value_num;
+                if (ob instanceof Array && value instanceof Array) { return ob.concat(value); }
+                return ob + value;
+            };
+        })
+
+        .filter('addslashes', function() {
+            return function(text) {
+                text = text.split("'").join("\'");
+                text = text.split('"').join('\"');
+                text = text.split('\\').join("\\\\");
+                return text;
             }
-            return text_arr2.join('');
-        };
-    })
-    .filter("join", function() {
-        return function(ob, join_text) {
-            if (!(ob instanceof Array)) { return ob; }
+        })
 
-            join_text = join_text || "";
-            return ob.join(join_text);
-        };
-    });
+        .filter('capfirst', function() {
+            return function(text) {
+                return text[0].toUpperCase() + text.substr(1);
+            }
+        })
+        .filter('center', function() {
+            return function(text, len) {
+                if (text.length > len) { return text; }
+                var diff = len - text.length;
+                var half_diff = Math.floor(diff/2);
+
+                var arr = [" "];
+                arr.length = half_diff;
+                var spaces = arr.join(" ");
+
+                if (half_diff * 2 < diff) { return spaces + " " + text + spaces; }
+                return spaces + text + spaces;
+            }
+        })
+        .filter('cut', function() {
+            return function(text, cut_text) {
+                return text.split(cut_text).join("");
+            };
+        })
+
+        .filter('default', function() {
+            return function(value, default_string) {
+                default_string = default_string || "---";
+                if (!value) { return default_string; }
+                return value;
+            };
+        })
+        .filter('default_if_none', function() {
+            return function(value, default_string) {
+                if (value === "") { return ""; }
+                default_string = default_string || "---";
+                if (!value) { return default_string; }
+                return value;
+            };
+        })
+
+        .filter('linebreak', function() {
+            //break_value - p, div, br
+            return function(text, break_value) { 
+                if (break_value === 'br') { return text.split(/\r|\n/).join("</br>"); }
+
+                var text_arr = text.split(/\r?\n/);
+                var text_arr2 = [];
+                for (var i = 0; i < text_arr.length; i++) {
+                    var text_item = text_arr[i];
+                    text_arr2.push("<" + break_value + ">" + text_item + "</" + break_value + ">");
+                }
+                return text_arr2.join('');
+            };
+        })
+        .filter("join", function() {
+            return function(ob, join_text) {
+                if (!(ob instanceof Array)) { return ob; }
+
+                join_text = join_text || "";
+                return ob.join(join_text);
+            };
+        });
+})();
--- tests/filtersSpec.js
+++ tests/filtersSpec.js
@@ -4,8 +4,65 @@
         var filter;
         beforeEach(module('useful_things'));
         beforeEach(inject(function ( $injector) {
-            filter = $injector.get('$filter')
+            filter = $injector.get('$filter');
         }));
+
+        it('log', function() {
+            filter('log')('test log filter');
+            expect(filter('log')('test log filter', true), '');
+        });
+
+        describe('add', function() {
+            it('add', function() {
+                expect(filter('add')('4', 2)).toBe(6);
+            });
+
+            it('add', function() {
+                expect(filter('add')('4', '2')).toBe(6);
+            });
+
+            it('add', function() {
+                expect(filter('add')('4ab', '2')).toBe('4ab2');
+            });
+
+            it('add', function() {
+                var a = [1,2,3];
+                var b = [4,5,6];
+                var result = filter('add')(a, b);
+                expect(result.length).toBe(6);
+                var i;
+                for (i = 0; i < a.length; i++) {
+                    expect(a[i]).toBe(result[i]);
+                }
+                for (i = 3; i < b.length; i++) {
+                    expect(b[i]).toBe(result[i]);
+                }
+            });
+        });
+
+        it('addslashes', function () {
+            expect(filter('addslashes')("I'm using Django")).toBe("I\'m using Django");
+            expect(filter('addslashes')('I"m using Django')).toBe('I\"m using Django');
+            expect(filter('addslashes')('\\ : backslashes, too')).toBe('\\\\ : backslashes, too')
+
+        });
+
+        it('capfirst', function() {
+            expect(filter('capfirst')('hello world')).toBe('Hello world');
+        });
+
+        it('center', function() {
+            expect(filter('center')('Django', 15)).toBe('     Django    ');
+            expect(filter('center')('Django', 18).length).toBe(18);
+        });
+
+        it('cut', function() {
+            expect(filter('cut')('String with spaces', ' ')).toBe('Stringwithspaces');
+        });
+
+        it('date', function() {
+            expect(true).toBe(false);
+        });
 
         it('default', function() {
             expect(filter('default')('')).toBe('---');
@@ -15,6 +72,220 @@
             expect(filter('default')('', 'default_value')).toBe('default_value');
             expect(filter('default')('have value', 'default_value')).toBe('have value');
         });
+        it('default_if_none', function() {
+            expect(filter('default_if_none')('')).toBe('');
+            expect(filter('default')(null)).toBe('---');
+        });
+
+
+
+        it('dictsort', function() {
+            expect(true).toBe(false);
+        });
+
+        it('dictsortreversed', function() {
+            expect(true).toBe(false);
+        });
+
+        it('divisibleby', function() {
+            expect(true).toBe(false);
+        });
+
+        it('escape', function() {
+            expect(true).toBe(false);
+        });
+
+        it('escapejs', function() {
+            expect(true).toBe(false);
+        });
+
+        it('filesizeformat', function() {
+            expect(true).toBe(false);
+        });
+
+        it('first', function() {
+            expect(true).toBe(false);
+        });
+
+        it('force_escape', function() {
+            expect(true).toBe(false);
+        });
+
+        it('get_digit', function() {
+            expect(true).toBe(false);
+        });
+
+        it('iriencode', function() {
+            expect(true).toBe(false);
+        });
+
+        it('join', function() {
+            expect(filter('join')([1,2,3], ", ")).toBe("1, 2, 3");
+            expect(filter('join')("one two theree", ", ")).toBe("one two theree");
+        });
+
+        it('last', function() {
+            expect(true).toBe(false);
+        });
+
+        it('length', function() {
+            expect(true).toBe(false);
+        });
+
+        it('length_is', function() {
+            expect(true).toBe(false);
+        });
+
+        it('linebreaks', function() {
+            expect(true).toBe(false);
+        });
+
+        it('linebreaksbr', function() {
+            expect(true).toBe(false);
+        });
+
+        it('linenumbers', function() {
+            expect(true).toBe(false);
+        });
+
+        it('ljust', function() {
+            expect(true).toBe(false);
+        });
+
+        it('lower', function() {
+            expect(true).toBe(false);
+        });
+
+        it('make_list', function() {
+            expect(true).toBe(false);
+        });
+
+        it('phone2numeric', function() {
+            expect(true).toBe(false);
+        });
+
+        it('pluralize', function() {
+            expect(true).toBe(false);
+        });
+
+        it('pprint', function() {
+            expect(true).toBe(false);
+        });
+
+        it('random', function() {
+            expect(true).toBe(false);
+        });
+
+        it('removetags', function() {
+            expect(true).toBe(false);
+        });
+
+        it('rjust', function() {
+            expect(true).toBe(false);
+        });
+
+        it('safe', function() {
+            expect(true).toBe(false);
+        });
+
+        it('safeseq', function() {
+            expect(true).toBe(false);
+        });
+
+        it('slice', function() {
+            expect(true).toBe(false);
+        });
+
+        it('slugify', function() {
+            expect(true).toBe(false);
+        });
+
+        it('stringformat', function() {
+            expect(true).toBe(false);
+        });
+
+
+        it('striptags', function() {
+            expect(true).toBe(false);
+        });
+
+
+        it('time', function() {
+            expect(true).toBe(false);
+        });
+
+        it('timesince', function() {
+            expect(true).toBe(false);
+        });
+
+
+        it('timeuntil', function() {
+            expect(true).toBe(false);
+        });
+
+        it('title', function() {
+            expect(true).toBe(false);
+        });
+
+        it('truncatechars', function() {
+            expect(true).toBe(false);
+        });
+
+        it('truncatechars_html', function() {
+            expect(true).toBe(false);
+        });
+
+        it('truncatewords', function() {
+            expect(true).toBe(false);
+        });
+
+        it('truncatewords_html', function() {
+            expect(true).toBe(false);
+        });
+
+        it('unordered_list', function() {
+            expect(true).toBe(false);
+        });
+
+        it('upper', function() {
+            expect(true).toBe(false);
+        });
+
+        it('urlencode', function() {
+            expect(true).toBe(false);
+        });
+
+        it('urlize', function() {
+            expect(true).toBe(false);
+        });
+
+        it('urlizetrunc', function() {
+            expect(true).toBe(false);
+        });
+
+        it('wordcount', function() {
+            expect(true).toBe(false);
+        });
+
+        it('wordwrap', function() {
+            expect(true).toBe(false);
+        });
+
+        it('yesno', function() {
+            expect(true).toBe(false);
+        });
+
+        it('i18n', function() {
+            expect(true).toBe(false);
+        });
+
+        it('l10n', function() {
+            expect(true).toBe(false);
+        });
+
+        it('tz', function() {
+            expect(true).toBe(false);
+        });
 
         it('linebreak', function() {
             expect(filter('linebreak')('text', 'p')).toBe('<p>text</p>');
@@ -22,14 +293,6 @@
             expect(filter('linebreak')('text\ntext', 'br')).toBe('text</br>text');
         });
         
-        it('log', function() {
-            filter('log')('test log filter');
-            expect(filter('log')('test log filter', true), '');
-        })
-        it('join', function() {
-            expect(filter('join')([1,2,3], ", ")).toBe("1, 2, 3");
-            expect(filter('join')("one two theree", ", ")).toBe("one two theree");
-        })
 
     }); 
 
